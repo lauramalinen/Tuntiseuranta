6df46c216c70b8e37514d1a588ea2e6ec1625c62
marjajohannaK <marjajkurkela@gmail.com>
2018-10-12 14:29:32 +0300

    Caset lisätty

--- Tuntiseuranta/Program.cs
+++ Tuntiseuranta/Program.cs
@@ -11,23 +11,43 @@ namespace Tuntiseuranta
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Tervetuloa!\nMit‰ haluat tehd‰?");
-            string vastaus = Console.ReadLine();
-            vastaus = vastaus.ToUpper();
-            switch (vastaus)
-            {
-                //case "A":
-                //    Kayttaja kayttaja = new Kayttaja();
-                //    kayttaja.LisaaKayttaja();
-                //    break;
-                case "TAULUKOT":
-                    Taulukot taulukot = new Taulukot();
-                    taulukot.PaivitaTaulukot();
-                    break;
-                default:
-                    Console.WriteLine("Ep‰kelpo valinta!");
-                    break;
-            }
+            //Start:
+            Console.WriteLine(@"Tervetuloa! Mit‰ haluat tehd‰?
+            A  Lis‰‰ uusi k‰ytt‰j‰
+            B  Lis‰‰ uusi tuntikirjaus
+            C  Tarkastele tuntikirjauksia");
+            //string vastaus = Console.ReadLine();
+            //vastaus = vastaus.ToUpper();
+            //switch (vastaus)
+            //{
+            //case "A":
+            //    Kayttaja kayttaja = new Kayttaja();
+            //    kayttaja.LisaaKayttaja();
+            //    break;
+
+            //case "B":
+            //    Tehtava tehtava = new Tehtava();
+            //    tehtava.LisaaTehtava();
+            //    break;
+
+            //case "C":
+            //    Katsele katsele = new Katsele;
+            //    katsele.TarkasteleTunteja();
+            //    break;
+
+            //default:
+            //    Console.WriteLine("Virheellinen valinta. Tee valinta uudelleen.");
+            //    break;
+
+            //case "TAULUKOT":
+            //    Taulukot taulukot = new Taulukot();
+            //    taulukot.PaivitaTaulukot();
+            //    break;
+            //default:
+            //    Console.WriteLine("Ep‰kelpo valinta! Kokeilehan uudelleen.");
+            //    goto Start;
+
+            //}
 
             //using (SqlConnection c = new SqlConnection())
             //{
