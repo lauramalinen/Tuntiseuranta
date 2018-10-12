9fd69ff2c30ee6c7425ef498b8b3a1d0a26bb4e8
marjajohannaK <marjajkurkela@gmail.com>
2018-10-12 13:33:37 +0300

    Testi

--- Tuntiseuranta/Program.cs
+++ Tuntiseuranta/Program.cs
@@ -1,9 +1,8 @@
 using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Tuntiseuranta
 {
@@ -11,19 +10,7 @@ namespace Tuntiseuranta
     {
         static void Main(string[] args)
         {
-
-            using (SqlConnection c = new SqlConnection())
-            {
-                c.ConnectionString = "server=localhost;database=Tuntiseuranta;trusted_connection=true";
-                c.Open();
-                SqlCommand cmd = c.CreateCommand();
-                cmd.CommandText = $"create table Kayttaja (kayttaja_id int, nimi varchar(50), osasto varchar(50), tehtavanimike varchar(50));";
-                cmd.ExecuteNonQuery();
-
-                SqlCommand cmd2 = c.CreateCommand();
-                cmd2.CommandText = $"create table Tunnit (tehtavanro int, kayttaja_id int, pvm date, tunnit decimal(4,2), tehtavakuvaus varchar(255), laskutettava int);";
-                cmd2.ExecuteNonQuery();
-            }
+            
         }
     }
 }
