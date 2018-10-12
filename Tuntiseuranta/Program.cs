9bffd84c3cd45f010a5e6e02b65b02bfe921ded4
forceberry <otto.forsberg@gmail.com>
2018-10-12 13:56:16 +0300

    Case lisätty

--- Tuntiseuranta/Program.cs
+++ Tuntiseuranta/Program.cs
@@ -12,22 +12,34 @@ namespace Tuntiseuranta
         static void Main(string[] args)
         {
             Console.WriteLine("Tervetuloa!\nMitä haluat tehdä?");
-            Console.ReadLine();
-
-            using (SqlConnection c = new SqlConnection())
+            string vastaus = Console.ReadLine();
+            vastaus.ToUpper();
+            switch (vastaus)
             {
-                //c.ConnectionString = "server=localhost;database=Tuntiseuranta;trusted_connection=true";
-                //c.Open();
-                //SqlCommand cmd = c.CreateCommand();
-                //cmd.CommandText = $"create table Kayttaja (kayttaja_id int, nimi varchar(50), osasto varchar(50), tehtavanimike varchar(50));";
-                //cmd.ExecuteNonQuery();
-
-                //SqlCommand cmd2 = c.CreateCommand();
-                //cmd2.CommandText = $"create table Tunnit (tehtavanro int, kayttaja_id int, pvm date, tunnit decimal(4,2), tehtavakuvaus varchar(255), laskutettava int);";
-                //cmd2.ExecuteNonQuery();
-
-                //Kommentti
+                //case "A":
+                //    Kayttaja kayttaja = new Kayttaja();
+                //    kayttaja.LisaaKayttaja();
+                //    break;
+                //case "TAULUKOT":
+                //    Taulukot taulukot = new Taulukot();
+                //    taulukot.PaivitaTaulukot();
+                //    break;
             }
+
+            //using (SqlConnection c = new SqlConnection())
+            //{
+            //    c.ConnectionString = "server=localhost;database=Tuntiseuranta;trusted_connection=true";
+            //    c.Open();
+            //    SqlCommand cmd = c.CreateCommand();
+            //    cmd.CommandText = $"create table Kayttaja (kayttaja_id int, nimi varchar(50), osasto varchar(50), tehtavanimike varchar(50));";
+            //    cmd.ExecuteNonQuery();
+
+            //    SqlCommand cmd2 = c.CreateCommand();
+            //    cmd2.CommandText = $"create table Tunnit (tehtavanro int, kayttaja_id int, pvm date, tunnit decimal(4,2), tehtavakuvaus varchar(255), laskutettava int);";
+            //    cmd2.ExecuteNonQuery();
+
+            //    Kommentti
+            //}
         }
     }
 }
