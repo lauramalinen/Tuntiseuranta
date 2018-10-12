9fce3efa0388fe6738295ba47442cd475d184f50
lauramalinen <laura.malinen@gmail.com>
2018-10-12 14:46:06 +0300

    kayttaja-luokan alkuja

--- Tuntiseuranta/Kayttaja.cs
+++ Tuntiseuranta/Kayttaja.cs
@@ -8,5 +8,19 @@ namespace Tuntiseuranta
 {
     class Kayttaja
     {
+        public void LisaaKayttaja()
+        {
+            Console.WriteLine("Lisää nimi: (etunimi sukunimi) ");
+            string nimi = Console.ReadLine();
+
+            Console.WriteLine("Lisää osasto: ");
+            string osasto = Console.ReadLine();
+
+            Console.WriteLine("Lisää tehtävänimike: ");
+            string tehtavanimike = Console.ReadLine();
+
+
+
+        }
     }
 }
