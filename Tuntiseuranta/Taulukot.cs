f78a2a1b4a788bfa5e197877dfbd5bff17747df9
forceberry <otto.forsberg@gmail.com>
2018-10-12 14:41:35 +0300

    Lisätty taulukoihin keyt

--- Tuntiseuranta/Taulukot.cs
+++ Tuntiseuranta/Taulukot.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,40 @@ namespace Tuntiseuranta
     {
         public void PaivitaTaulukot()
         {
-            Console.WriteLine("OK");
+            Console.WriteLine("Valitse toiminto");
+            Console.WriteLine("A: Päivitä primary ja secondary keyt");
+            string vastaus = Console.ReadLine();
+            vastaus = vastaus.ToUpper();
+            switch (vastaus)
+            {
+                case "A":
+                    using (SqlConnection c = new SqlConnection())
+                    {
+                        c.ConnectionString = "server=localhost;database=Tuntiseuranta;trusted_connection=true";
+                        c.Open();
+                        SqlCommand a1 = c.CreateCommand();
+                        a1.CommandText = $"ALTER TABLE Kayttaja ALTER COLUMN kayttaja_id int NOT NULL";
+                        SqlCommand a2 = c.CreateCommand();
+                        a2.CommandText = $"ALTER TABLE Tunnit ALTER COLUMN tehtavanro int NOT NULL";
+                        SqlCommand c1 = c.CreateCommand();
+                        c1.CommandText = $"ALTER TABLE Kayttaja ADD PRIMARY KEY (kayttaja_id);";
+                        SqlCommand c2 = c.CreateCommand();
+                        c2.CommandText = $"ALTER TABLE Tunnit ADD PRIMARY KEY (tehtavanro);";
+                        SqlCommand c3 = c.CreateCommand();
+                        c3.CommandText = $"ALTER TABLE Tunnit ADD FOREIGN KEY(kayttaja_id) REFERENCES Kayttaja(kayttaja_id);";
+                        a1.ExecuteNonQuery();
+                        a2.ExecuteNonQuery();
+                        c1.ExecuteNonQuery();
+                        c2.ExecuteNonQuery();
+                        c3.ExecuteNonQuery();
+                    }
+                break;
+                default:
+                    Console.WriteLine("Epäkelpo valinta!");
+                    PaivitaTaulukot();
+                    break;
+            }
+            Console.WriteLine("OK!");
         }
     }
 }
