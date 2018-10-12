b8b29973d6be4553444070b61ae75bc828abb100
forceberry <otto.forsberg@gmail.com>
2018-10-12 13:28:56 +0300

    Kenttien luonti

--- Tuntiseuranta/Program.cs
+++ Tuntiseuranta/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,19 @@ namespace Tuntiseuranta
     {
         static void Main(string[] args)
         {
-            
+
+            using (SqlConnection c = new SqlConnection())
+            {
+                c.ConnectionString = "server=localhost;database=Tuntiseuranta;trusted_connection=true";
+                c.Open();
+                SqlCommand cmd = c.CreateCommand();
+                cmd.CommandText = $"create table Kayttaja (kayttaja_id int, nimi varchar(50), osasto varchar(50), tehtavanimike varchar(50));";
+                cmd.ExecuteNonQuery();
+
+                SqlCommand cmd2 = c.CreateCommand();
+                cmd2.CommandText = $"create table Tunnit (tehtavanro int, kayttaja_id int, pvm date, tunnit decimal(4,2), tehtavakuvaus varchar(255), laskutettava int);";
+                cmd2.ExecuteNonQuery();
+            }
         }
     }
 }
