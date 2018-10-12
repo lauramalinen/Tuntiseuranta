f9044caf73a52d1149c4d477a68e50ca8c2a6759
MChorin <miika.chorin@gmail.com>
2018-10-12 16:15:57 +0300

    Testi

--- Tuntiseuranta/Kayttaja.cs
+++ Tuntiseuranta/Kayttaja.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+
 
 namespace Tuntiseuranta
 {
     class Kayttaja
     {
+       
+
     }
+    
+    
 }
