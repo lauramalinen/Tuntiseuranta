ecfc9007ea3c5fbf3fe8c09a1476ec4c176cd096
lauramalinen <laura.malinen@gmail.com>
2018-10-12 15:19:43 +0300

    kayttaja-luokan alkuja

--- Tuntiseuranta/Kayttaja.cs
+++ Tuntiseuranta/Kayttaja.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,30 @@ namespace Tuntiseuranta
 {
     class Kayttaja
     {
+        public void LisaaKayttaja()
+        {
+            Console.WriteLine("Lisää nimi: (etunimi sukunimi) ");
+            string nimi = Console.ReadLine();
+
+            Console.WriteLine("Lisää osasto: ");
+            string osasto = Console.ReadLine();
+
+            Console.WriteLine("Lisää tehtävänimike: ");
+            string tehtavanimike = Console.ReadLine();
+
+
+
+            using (SqlConnection c = new SqlConnection())
+            {
+                c.ConnectionString = "server=localhost;database=Tuntiseuranta;trusted_connection=true";
+                c.Open();
+                SqlCommand k1 = c.CreateCommand();
+                k1.CommandText = $"insert into Kayttaja values({nimi}, {osasto}, {tehtavanimike}";
+
+                k1.ExecuteNonQuery();
+
+
+            }
+        }
     }
 }
