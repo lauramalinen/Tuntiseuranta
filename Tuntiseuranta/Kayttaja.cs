faf74c4e04f289e3e0c1f96fb9393b5dc62d0c84
forceberry <otto.forsberg@gmail.com>
2018-10-12 16:12:57 +0300

    Try-catch lisätty Käyttäjä-metodiin

--- Tuntiseuranta/Kayttaja.cs
+++ Tuntiseuranta/Kayttaja.cs
@@ -11,24 +11,41 @@ namespace Tuntiseuranta
     {
         public void LisaaKayttaja()
         {
-            Console.WriteLine("Lisää nimi: (etunimi sukunimi) ");
-            string nimi = Console.ReadLine();
-
-            Console.WriteLine("Lisää osasto: ");
-            string osasto = Console.ReadLine();
-
-            Console.WriteLine("Lisää tehtävänimike: ");
-            string tehtavanimike = Console.ReadLine();
-
-            using (SqlConnection c = new SqlConnection())
+            try
             {
-                c.ConnectionString = "server=localhost;database=Tuntiseuranta;trusted_connection=true";
-                c.Open();
-                SqlCommand k1 = c.CreateCommand();
-                k1.CommandText = $"insert into Kayttaja values('{nimi}', '{osasto}', '{tehtavanimike}')";
+                Console.WriteLine("Lisää nimi: (etunimi sukunimi) ");
+                string nimi = Console.ReadLine();
 
-                k1.ExecuteNonQuery();
+                Console.WriteLine("Lisää osasto: ");
+                string osasto = Console.ReadLine();
 
+                Console.WriteLine("Lisää tehtävänimike: ");
+                string tehtavanimike = Console.ReadLine();
+
+                using (SqlConnection c = new SqlConnection())
+                {
+                    c.ConnectionString = "server=localhost;database=Tuntiseuranta;trusted_connection=true";
+                    c.Open();
+                    SqlCommand k1 = c.CreateCommand();
+                    k1.CommandText = $"insert into Kayttaja values('{nimi}', '{osasto}', '{tehtavanimike}')";
+
+                    k1.ExecuteNonQuery();
+
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Joku meni pieleen!\nKokeile uudestaan? K/E");
+                string vastaus = Console.ReadLine();
+                vastaus = vastaus.ToUpper();
+                switch (vastaus)
+                {
+                    case "K":
+                        LisaaKayttaja();
+                        break;
+                    default:
+                        break;
+                }
             }
         }
     }
--- Tuntiseuranta/Program.cs
+++ Tuntiseuranta/Program.cs
@@ -48,22 +48,6 @@ namespace Tuntiseuranta
             goto Start;
 
             }
-
-            //using (SqlConnection c = new SqlConnection())
-            //{
-            //    c.ConnectionString = "server=localhost;database=Tuntiseuranta;trusted_connection=true";
-            //    c.Open();
-            //    SqlCommand cmd = c.CreateCommand();
-            //    cmd.CommandText = $"create table Kayttaja (kayttaja_id int, nimi varchar(50), osasto varchar(50), tehtavanimike varchar(50));";
-            //    cmd.ExecuteNonQuery();
-
-            //    SqlCommand cmd2 = c.CreateCommand();
-            //    cmd2.CommandText = $"create table Tunnit (tehtavanro int, kayttaja_id int, pvm date, tunnit decimal(4,2), tehtavakuvaus varchar(255), laskutettava int);";
-            //    cmd2.ExecuteNonQuery();
-
-            //    Kommentti
-            //}
-
             Console.ReadLine();
         }
     }
